4f46f66cd77be6397c2ad9ff1fb177a304fb91fd
Omasyo <cesare3000@outlook.com>
2023-05-14 15:40:48 +0100

    advanced opengl

---
+++ AdvancedOpenGL/Mesh.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <vector>
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
+#include "Shader.h"
+
+enum class TextureType { diffuse, specualar };
+
+struct Vertex 
+{
+	glm::vec3 position;
+	glm::vec3 normal;
+	glm::vec2 texCoords;
+};
+
+struct Texture
+{
+	GLuint id;
+	TextureType type;
+	std::string path;
+};
+
+class Mesh
+{
+public:
+	std::vector<Vertex> vertices;
+	std::vector<GLuint> indices;
+	std::vector<Texture> textures;
+
+	Mesh(
+		std::vector<Vertex> vertices,
+		std::vector<GLuint> indices,
+		std::vector<Texture> textures
+	);
+
+	void draw(Shader& shader);
+
+private:
+	GLuint VAO, VBO, EBO;
+
+	void setupMesh();
+};
+
